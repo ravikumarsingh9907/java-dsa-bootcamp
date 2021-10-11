cab3878b6049a75a7189d8ff83c2fc52c45e7d35
Ravi Kumar <ravikumarsingh9907@gmail.com>
2021-10-12 00:33:54 +0530

    Update Patterns.java

--- 09-patterns/Patterns.java
+++ 09-patterns/Patterns.java
@@ -57,6 +57,64 @@ public class Patterns {
         System.out.println();
     }
     System.out.println("----------------------------------------------------------------");
-
+    /*
+    5.  *
+        **
+        ***
+        ****
+        *****
+        ****
+        ***
+        **
+        *
+    */
+     for(int row=1;row<=2*n;row++){
+            if(row <= n) {
+                for(int col=1;col<=row;col++){
+                    System.out.print("* ");
+                }
+            }else {
+                for (int col = 1; col <= 2*n-row; col++) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    System.out.println("----------------------------------------------------------------");
+    /*
+    6.   *
+        **
+       ***
+      ****
+     *****
+    */
+     for(int row=1;row<=n;row++){
+            for(int col=n;col>=1;col--){
+                if(col>row){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    System.out.println("----------------------------------------------------------------");
+    /*
+    7. *****
+        ****
+         ***
+          **
+           *
+    */
+     for (int row=0;row<=n;row++){
+            for(int space=0;space<=row;space++){
+                System.out.print(" ");
+            }
+            for(int col=n; col>=row;col--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    System.out.println("----------------------------------------------------------------"); 
     }
 }
