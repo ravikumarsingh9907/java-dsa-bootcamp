92bd413aad26cf3e43779344599d0cd923b51a5d
Ravi Kumar <ravikumarsingh9907@gmail.com>
2021-10-14 16:12:12 +0530

    Update Patterns.java

--- 09-patterns/Patterns.java
+++ 09-patterns/Patterns.java
@@ -136,5 +136,43 @@ public class Patterns {
             System.out.println();
         }
      System.out.println("----------------------------------------------------------------");
+     /*
+     9.  *********
+          *******
+           *****
+            ***
+             *
+     */
+      int k = 0;
+        for (int i = row; i >= 1; --i, k = 0) {
+            for (int space = row-i; space >=1; --space) {
+                System.out.print("  ");
+            }
+            while (k != 2 * i - 1) {
+                System.out.print("* ");
+                ++k;
+            }
+
+            System.out.println();
+        }
+     System.out.println("----------------------------------------------------------------");
+     /*
+     10.      *
+             * *
+            * * *
+           * * * *
+          * * * * *
+     */
+     for(int row=1;row<=n;row++){
+            for(int col=n;col>=1;col--){
+                if(col>row){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    System.out.println("----------------------------------------------------------------");
     }
 }
